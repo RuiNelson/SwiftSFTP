b3a687c683cc03acd8165df2c97c4e7cbac52f97
Rui Nelson <7100905+RuiNelson@users.noreply.github.com>
2026-06-01 16:58:48 +0100

    refactor: move LibSSH2Session extensions

--- Sources/SwiftSFTP/Wrappers/Support.swift
+++ Sources/SwiftSFTP/Wrappers/Support.swift
@@ -9,36 +9,6 @@ extension Int32 {
     }
 }
 
-extension LibSSH2Session {
-    func checkReturnValue(_ code: Int32) throws {
-        if code < 0 {
-            throw LibSSH2Error(code: code, message: lastErrorMessage)
-        }
-    }
-    
-    func checkCount(_ code: Int32) throws -> Int {
-        if code < 0 {
-            throw LibSSH2Error(code: code, message: lastErrorMessage)
-        }
-        return Int(code)
-    }
-    
-    func checkSize(_ size: Int) throws -> Int {
-        if size < 0 {
-            throw LibSSH2Error(code: Int32(size), message: lastErrorMessage)
-        }
-        return size
-    }
-    
-    var lastErrorMessage: String? {
-        var messagePointer: UnsafeMutablePointer<CChar>?
-        var messageLength: Int32 = 0
-        _ = libssh2.libssh2_session_last_error(rawValue, &messagePointer, &messageLength, 0)
-        guard let messagePointer else { return nil }
-        return String(cString: messagePointer)
-    }
-}
-
 extension UnsafePointer<CChar> {
     var string: String {
         String(cString: self)
--- Sources/SwiftSFTP/Wrappers/Types/LibSSH2Session.swift
+++ Sources/SwiftSFTP/Wrappers/Types/LibSSH2Session.swift
@@ -7,3 +7,33 @@ public struct LibSSH2Session {
         self.rawValue = rawValue
     }
 }
+
+extension LibSSH2Session {
+    func checkReturnValue(_ code: Int32) throws {
+        if code < 0 {
+            throw LibSSH2Error(code: code, message: lastErrorMessage)
+        }
+    }
+    
+    func checkCount(_ code: Int32) throws -> Int {
+        if code < 0 {
+            throw LibSSH2Error(code: code, message: lastErrorMessage)
+        }
+        return Int(code)
+    }
+    
+    func checkSize(_ size: Int) throws -> Int {
+        if size < 0 {
+            throw LibSSH2Error(code: Int32(size), message: lastErrorMessage)
+        }
+        return size
+    }
+    
+    var lastErrorMessage: String? {
+        var messagePointer: UnsafeMutablePointer<CChar>?
+        var messageLength: Int32 = 0
+        _ = libssh2.libssh2_session_last_error(rawValue, &messagePointer, &messageLength, 0)
+        guard let messagePointer else { return nil }
+        return String(cString: messagePointer)
+    }
+}
